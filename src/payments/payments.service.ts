ad96df3551ea7344885b1ce6a36264c1509b0dfb
Konstantinos Kampourelis <konkamp94@gmail.com>
2024-10-23 00:31:49 +0300

    payment service and dto validator

--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -1,4 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
+import { BaseChargeCardDto, PinPaymentsChargeCardDto, StripeChargeCardDto } from './dto/charge-card.dto';
+import { PspFactory } from 'src/psp/psp-implementations/psp.factory';
+import { MerchantCustomRepository } from 'src/merchant/repository/merchant-custom-repository.interface';
+import { CustomRepository } from 'src/shared/repository/custom-repository.interface';
+import { MerchantPsp } from 'src/merchant/entities/merchant-psp.entity';
+import { EnabledPsp } from './types/enabled-psp.type';
+import { ConfigService } from '@nestjs/config';
+import { PspResponse } from 'src/psp/psp-implementations/types/psp-response.type';
 
 @Injectable()
-export class PaymentsService {}
+export class PaymentsService {
+    constructor(
+        private configService: ConfigService,
+        @Inject('MERCHANT_CUSTOM_REPOSITORY') private merchantRepository: MerchantCustomRepository,
+        @Inject('CUSTOM_REPOSITORY') private merchantPspRepository: CustomRepository<MerchantPsp>,
+    ) { }
+
+    async findEnabledPsp(merchantId: number): Promise<EnabledPsp> {
+        const merchantPsp = await this.merchantRepository.findEnabledPspByMerchantId(merchantId);
+
+        if (!merchantPsp) {
+            throw new Error('No enabled PSP found for this merchant');
+        }
+
+        return { name: merchantPsp.psp.name, secretKey: merchantPsp.secretKey, publicKey: merchantPsp.publicKey };
+    }
+
+    async chargeCard(chargeCardDto: BaseChargeCardDto, enabledPsp: EnabledPsp): Promise<PspResponse> {
+        const { name, secretKey, publicKey } = enabledPsp;
+        return PspFactory.getPspInstance(name, {
+            secretKey,
+            publicKey
+        }, this.configService).paymentWorkflow(chargeCardDto);
+
+    };
+
+}
---
+++ src/payments/validators/psp-charge-card-dto.validator.ts
@@ -0,0 +1,29 @@
+import { BadRequestException } from "@nestjs/common";
+import { StripeCardInfo, PinPaymentsChargeCardDto, StripeChargeCardDto, PinPaymentsCardInfo, BaseChargeCardDto } from "../dto/charge-card.dto";
+import { validate } from "class-validator";
+
+const PspNameToDto = {
+    'Stripe': { dto: StripeChargeCardDto, cardInfo: StripeCardInfo },
+    'Pin Payments': { dto: PinPaymentsChargeCardDto, cardInfo: PinPaymentsCardInfo }
+}
+
+export const pspChargeCardDtoValidator = async (chargeCardDto: BaseChargeCardDto, pspName: string) => {
+    const dtoInstance = new PspNameToDto[pspName].dto();
+    const cardInfoInstance = new PspNameToDto[pspName].cardInfo();
+
+    Object.assign(dtoInstance, chargeCardDto);
+    // workaround to validate nested object properties
+    Object.assign(cardInfoInstance, chargeCardDto.cardInfo);
+    dtoInstance.cardInfo = cardInfoInstance;
+    console.log(dtoInstance);
+    const errors = await validate(dtoInstance);
+    if (errors.length > 0) {
+        const formattedErrors = errors.map(error => {
+            return { [error.property]: error.children?.map(child => child.constraints) };
+        });
+        throw new BadRequestException({
+            message: `${pspName} PSP is enabled for your merchant. Please review the following errors`,
+            errors: JSON.stringify(formattedErrors).replace(/"/g, '')
+        });
+    }
+}
