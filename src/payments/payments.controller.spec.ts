7d9ddc29a887ca720021991da1e37939b49f94cb
Konstantinos Kampourelis <konkamp94@gmail.com>
2024-10-24 20:01:27 +0300

    unit test for payments service and controller

--- src/payments/payments.controller.spec.ts
+++ src/payments/payments.controller.spec.ts
@@ -1,14 +1,48 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PaymentsController } from './payments.controller';
 import { PaymentsService } from './payments.service';
+import { RequestWithMerchant } from './types/request.type';
+import { BadRequestException, HttpException, InternalServerErrorException } from '@nestjs/common';
+import { pspChargeCardDtoValidator } from './validators/psp-charge-card-dto.validator';
+import { EnabledPsp } from './types/enabled-psp.type';
+
+jest.mock('./validators/psp-charge-card-dto.validator');
 
 describe('PaymentsController', () => {
   let controller: PaymentsController;
+  let mockMerchantRepository = {
+    findEnabledPspByMerchantId: jest.fn(),
+    findByClientId: jest.fn()
+  }
+  let mockPaymentsService = {
+    findEnabledPsp: jest.fn(),
+    chargeCard: jest.fn()
+  }
 
-  beforeEach(async () => {
+  let mockRequest = { merchant: { id: 1 } } as RequestWithMerchant;
+  let mockChargeCardDto = {
+    "cardInfo": {
+      "cardNumber": "4242424242424242",
+      "expirationDate": "08/25",
+      "cvv": "312"
+    },
+    "amount": 1000,
+    "currency": "usd"
+  };
+  let enabledPsp: EnabledPsp = { name: 'Stripe', secretKey: 'secret', publicKey: 'public' };
+
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PaymentsController],
-      providers: [PaymentsService],
+      providers: [{
+        provide: PaymentsService,
+        useValue: mockPaymentsService
+      },
+      {
+        provide: 'MERCHANT_CUSTOM_REPOSITORY',
+        useValue: mockMerchantRepository
+      }
+      ],
     }).compile();
 
     controller = module.get<PaymentsController>(PaymentsController);
@@ -16,5 +50,60 @@ describe('PaymentsController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should charge card', async () => {
+    const pspResponse = { type: 'success', pspData: {}, paymentStatus: 'success' };
+    mockPaymentsService.findEnabledPsp.mockResolvedValueOnce(enabledPsp);
+    mockPaymentsService.chargeCard.mockResolvedValueOnce(pspResponse);
+    const response = await controller.chargeCard(mockRequest, mockChargeCardDto);
+
+    expect(response).toEqual({ pspData: pspResponse.pspData, paymentStatus: pspResponse.paymentStatus });
+  });
+
+  it('should return a response with status code 400 when psp service returns error response with status 400', async () => {
+    const pspResponse = { type: 'error', message: 'error', statusCode: 400 };
+    mockPaymentsService.findEnabledPsp.mockResolvedValueOnce(enabledPsp);
+    mockPaymentsService.chargeCard.mockResolvedValueOnce(pspResponse);
+
+    await expect(controller.chargeCard(mockRequest, mockChargeCardDto))
+      .rejects.toThrow(new BadRequestException({ message: pspResponse.message, statusCode: pspResponse.statusCode }));
+
+  });
+
+  it('should return a response with status code 502 when psp service returns error response with status code 500', async () => {
+    const pspResponse = { type: 'error', message: 'error', statusCode: 500 };
+    mockPaymentsService.findEnabledPsp.mockResolvedValueOnce(enabledPsp);
+    mockPaymentsService.chargeCard.mockResolvedValueOnce(pspResponse);
+
+    await expect(controller.chargeCard(mockRequest, mockChargeCardDto))
+      .rejects.toThrow(new HttpException({ message: pspResponse.message, statusCode: pspResponse.statusCode }, 502));
+
+  });
+
+  it('should throw internal server error if no enabled PSP found', async () => {
+    const errorMessage = 'No enabled PSP found for this merchant. Contact support';
+    mockPaymentsService.findEnabledPsp.mockRejectedValueOnce(new Error(errorMessage));
+
+    await expect(controller.chargeCard(mockRequest, mockChargeCardDto))
+      .rejects.toThrow(new InternalServerErrorException(errorMessage));
+  });
+
+  it('should throw internal server error if payments service method charge card fails', async () => {
+    const errorMessage = 'Something went wrong. Payment failed';
+    mockPaymentsService.findEnabledPsp.mockResolvedValueOnce(enabledPsp);
+    mockPaymentsService.chargeCard.mockRejectedValueOnce(new Error());
+
+    await expect(controller.chargeCard(mockRequest, mockChargeCardDto))
+      .rejects.toThrow(new InternalServerErrorException(errorMessage));
+  });
+
+  it('should return a response with status code 400 if validator throws BadRequestException', async () => {
+    const errorFormat = { message: 'error', errors: 'error_array' };
+    mockPaymentsService.findEnabledPsp.mockResolvedValueOnce(enabledPsp);
+    (pspChargeCardDtoValidator as jest.Mock).mockRejectedValueOnce(new BadRequestException(errorFormat));
+
+    await expect(controller.chargeCard(mockRequest, mockChargeCardDto))
+      .rejects.toThrow(new BadRequestException(errorFormat));
   });
 });
--- src/payments/payments.service.spec.ts
+++ src/payments/payments.service.spec.ts
@@ -1,18 +1,114 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PaymentsService } from './payments.service';
+import { ConfigService } from '@nestjs/config';
+import { EnabledPsp } from './types/enabled-psp.type';
+import { PspFactory } from '../psp/psp-implementations/psp.factory';
+import { BaseChargeCardDto } from './dto/charge-card.dto';
+import { PspResponse } from 'src/psp/psp-implementations/types/psp-response.type';
 
 describe('PaymentsService', () => {
-  let service: PaymentsService;
+  let paymentsService: PaymentsService;
+  let mockMerchantRepository: { findEnabledPspByMerchantId: jest.Mock };
+  let mockPspFactory: { getPspInstance: jest.Mock };
+  beforeAll(async () => {
 
-  beforeEach(async () => {
+    mockMerchantRepository = { findEnabledPspByMerchantId: jest.fn() };
+    mockPspFactory = { getPspInstance: jest.fn() };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PaymentsService],
+      providers: [
+        PaymentsService,
+        {
+          provide: 'MERCHANT_CUSTOM_REPOSITORY',
+          useValue: mockMerchantRepository,
+        },
+        {
+          provide: PspFactory,
+          useValue: mockPspFactory,
+        }
+      ],
     }).compile();
 
-    service = module.get<PaymentsService>(PaymentsService);
+    paymentsService = module.get<PaymentsService>(PaymentsService);
+
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(paymentsService).toBeDefined();
+  });
+
+  describe('findEnabledPsp', () => {
+    it('should return enabled PSP', async () => {
+      const merchantId = 1;
+      const merchantPspWithPspDetails = {
+        id: 12,
+        enabled: true,
+        secretKey: 'secret_key',
+        publicKey: 'public_key',
+        merchantId: 50,
+        pspId: 46,
+        psp: { id: 46, name: 'Stripe', createdAt: new Date() }
+      }
+
+      mockMerchantRepository.findEnabledPspByMerchantId.mockResolvedValue(merchantPspWithPspDetails);
+
+      const enabledPsp: EnabledPsp = await paymentsService.findEnabledPsp(merchantId);
+
+      expect(mockMerchantRepository.findEnabledPspByMerchantId).toHaveBeenCalledWith(merchantId);
+      expect(enabledPsp).toEqual({
+        name: 'Stripe', secretKey: 'secret_key', publicKey: 'public_key'
+      });
+
+    });
+
+    it('should throw an error if no enabled PSP found', async () => {
+      const merchantId = 1;
+
+      mockMerchantRepository.findEnabledPspByMerchantId.mockResolvedValue(null);
+      await expect(paymentsService.findEnabledPsp(merchantId))
+        .rejects.toThrow('No enabled PSP found for this merchant');
+
+    });
+  });
+
+  describe('chargeCard', () => {
+    it('should return PSP response', async () => {
+      const chargeCardDto: BaseChargeCardDto = {
+        cardInfo: {
+          cardNumber: "4242424242424242",
+          expirationDate: "08/25",
+          cvv: "312"
+        },
+        amount: 1000,
+      }
+      const enabledPsp: EnabledPsp = {
+        name: 'Stripe',
+        secretKey: 'secret key',
+        publicKey: 'public key',
+      }
+
+      const pspResponse: PspResponse = {
+        type: 'success',
+        paymentStatus: 'succeeded',
+        pspData: { status: 'succeeded' }
+      }
+
+      const mockPspServiceInstance = {
+        paymentWorkflow: jest.fn().mockReturnValueOnce(pspResponse)
+      };
+      mockPspFactory.getPspInstance.mockReturnValueOnce(mockPspServiceInstance);
+
+      const response = await paymentsService.chargeCard(chargeCardDto, enabledPsp);
+
+      expect(mockPspFactory.getPspInstance)
+        .toHaveBeenCalledWith(enabledPsp.name, { secretKey: enabledPsp.secretKey, publicKey: enabledPsp.publicKey });
+      expect(mockPspServiceInstance.paymentWorkflow).toHaveBeenCalledWith(chargeCardDto);
+      expect(response).toEqual({ type: 'success', paymentStatus: 'succeeded', pspData: { status: 'succeeded' } });
+    });
+
+    it('should throw an error if PSP response is not successful', async () => {
+
+    });
+
   });
 });
